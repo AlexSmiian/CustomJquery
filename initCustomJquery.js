2f78b0c7bbc986b93dc134a7ec940dad909467e5
AlexSmiian <62544817+AlexSmiian@users.noreply.github.com>
2024-09-18 09:57:27 +0300

    Update initCustomJquery.js

--- initCustomJquery.js
+++ initCustomJquery.js
@@ -1,30 +1,83 @@
-const $ = (selector) => {
-    const elements = document.querySelectorAll(selector);
+const $ = (selectorOrElement) => {
+        if (typeof selectorOrElement === 'string') {
+        const elements = document.querySelectorAll(selectorOrElement);
 
-    return elements.length === 1 ? elements[0] : elements;
+        if (elements.length === 0) {
+            return undefined;
+        }
+
+        const isSingleElement = elements.length === 1;
+        const elementArray = isSingleElement ? [elements[0]] : Array.from(elements);
+
+        return {
+            elementArray,
+
+            addClass(className) {
+                this.elementArray.forEach(element => {
+                    element.classList.add(className);
+                });
+                return this; 
+            },
+
+            removeClass(className) {
+                this.elementArray.forEach(element => {
+                    element.classList.remove(className);
+                });
+                return this; 
+            },
+
+            onClick(callback) {
+                this.elementArray.forEach(element => {
+                    element.addEventListener('click', (event) => {
+                        const clickedElement = event.currentTarget;
+                        callback(clickedElement);
+                    });
+                });
+                return this; 
+            }
+        };
+    } else if (selectorOrElement instanceof Element) {
+
+        return {
+            selectorOrElement,
+
+            addClass(className) {
+                this.selectorOrElement.classList.add(className);
+             
+                return this; 
+            },
+
+            removeClass(className) {
+                this.selectorOrElement.classList.remove(className);
+                return this; 
+            },
+
+            onClick(callback) {
+                this.selectorOrElement.forEach(element => {
+                    element.addEventListener('click', (event) => {
+                        const clickedElement = event.currentTarget;
+                        callback(clickedElement);
+                    });
+                });
+                return this; 
+            }
+        };
+    } else {
+        return undefined;
+    }
 };
 
-const addClass = (element, className) => {
-        element.classList.add(className);
-};
+const activeClass = '--active';
+const activeGreen = '--active-green';
 
-const removeClass = (element, className) => {
-        element.classList.remove(className);
-};
+$('.item')
+    .onClick((clickedElement) => {
+        $(clickedElement).addClass(activeGreen);
+    });
 
-const toggle = (item,className) => {
-    item.classList.contains(className) ? removeClass(item,className) : addClass(item,className);
-}
+$('.item-custom').removeClass(activeClass).addClass(activeGreen);
 
-// Exlample
-const activeClass = '--active'
 
-console.log($('.item-custom'));
-console.log($('#wrapper-id'))
-console.log($('[data-item]'))
-
-$('.item-custom').forEach(function(item) {
-    item.addEventListener('click',() => {
-        toggle(item, activeClass); 
-    })
-})
+console.log($('#wrapper-id'));
+console.log($('.item'));
+console.log($('.itemsadsd'));
